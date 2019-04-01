711e52443f937cb4f2acd0e8b37303e1807d568d
preke <geek00021@gmail.com>
2019-04-01 16:32:02 +0800

    .

--- train.py
+++ train.py
@@ -136,8 +136,8 @@ def main(args):
                 print('ELBO:\n')
                 print(tracker['ELBO'])
                 print('----')
-                print(loss.data.unsqeeze())
-                tracker['ELBO'] = torch.cat((tracker['ELBO'], loss.data.unsqeeze()))
+                print(loss.data.unsqueeze())
+                tracker['ELBO'] = torch.cat((tracker['ELBO'], loss.data.unsqueeze()))
 
                 if args.tensorboard_logging:
                     writer.add_scalar("%s/ELBO"%split.upper(), loss.data[0], epoch*len(data_loader) + iteration)
