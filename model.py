ff911b7adc6c9193440c4208db41b3967f205be5
preke <geek00021@gmail.com>
2019-04-01 21:35:44 +0800

    .

--- model.py
+++ model.py
@@ -168,9 +168,9 @@ class SentenceVAE(nn.Module):
             # print(running_seqs)
             if len(running_seqs) > 0:
                 # print('fff')
-                # print(running_seqs)
-                # print('\n')
-                # print(input_sequence)
+                print(running_seqs)
+                print('\n')
+                print(input_sequence)
                 input_sequence = input_sequence[running_seqs]
                 hidden = hidden[:, running_seqs]
 
