656c99829af45287b78f24fbb08f09e902dc7c29
preke <geek00021@gmail.com>
2019-04-02 10:10:01 +0800

    .

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import json
 
 
-with open('dumps/2019-Apr-01-16:33:42/valid_E99.json', 'r') as file:
+with open('dumps/2019-Apr-01-16:33:42/valid_E50.json', 'r') as file:
     sentences = json.load(file)
 
 target = sentences['target_sents'][:20]
