2587bb52155ecb419d6a8ea34c9f5c71f36a8443
preke <geek00021@gmail.com>
2019-04-02 10:03:42 +0800

    .

---
+++ check.py
@@ -0,0 +1,12 @@
+import json
+
+
+with open('dumps/2019-Apr-01-16:33:42/valid_E99.json', 'r') as file:
+    sentences = json.load(file)
+
+target = sentences['target_sents'][:20]
+gen = sentences['gen_sents'][:20]
+for i in range(len(target)):
+	print(target[i])
+	print(gen[i])
+	print('\n')
